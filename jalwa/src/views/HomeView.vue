ff51d1df93805640c9109b2cac0a9f80f0f60b1e
Arshdeep Sangha <asangha@local.sequiter.com>
2025-03-02 11:50:44 -0700

    set app base and remove unwanted stuff

--- jalwa/src/views/HomeView.vue
+++ jalwa/src/views/HomeView.vue
@@ -1,9 +1,28 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="home-view">
+        <h1>Select your file</h1>
+        <div>
+            <input type="file"
+                @change="onFileChanged($event)"
+                accept="image/*"
+                capture
+                />
+        </div>
+    </div>
 </template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+const selectedImage = ref<File | null>(null)
+
+const onFileChanged = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  const file = target.files?.[0]
+  console.log(file)
+  if (file) {
+    selectedImage.value = file
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
